1b915d0a6dae18741b6ea24453d739924fdfc759
arielkru <63583491+arielkru@users.noreply.github.com>
2020-11-23 22:00:25 +0200

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -86,3 +86,17 @@ resource "aws_s3_bucket" "logs" {
     Environment = local.resource_prefix.value
   }
 }
+
+resource "aws_s3_bucket" "financials 2" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-financials"
+  acl           = "private"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-financials"
+    Environment = local.resource_prefix.value
+  }
+
+}
