dc5e5a29772b7ccb08e8bb1d9ab8b181c6380cf2
jonnyCodev <42693227+jonnyCodev@users.noreply.github.com>
2021-04-20 14:24:03 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -6,6 +6,7 @@ resource "aws_s3_bucket" "data" {
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "public-read"
   force_destroy = true
+  test          = "yoni"
   tags = {
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
