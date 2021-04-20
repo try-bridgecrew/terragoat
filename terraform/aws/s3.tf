c070d7742803effdc1d2c256a219753cd7f645af
Tommy Song <tommynsong@gmail.com>
2021-04-19 23:22:21 -0500

    add versioning and sse

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -96,8 +96,18 @@ resource "aws_s3_bucket" "demo" {
     Name        = "${local.resource_prefix.value}-demo"
     Environment = local.resource_prefix.value
   }
+  versioning {
+    enabled = true
+  }
   logging {
     target_bucket = "${aws_s3_bucket.logs.id}"
     target_prefix = "demo/"
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
   }
 }
